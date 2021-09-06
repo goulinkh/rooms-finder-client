d4cab7880ddd20bfb7b1ccab6810dcff04122b44
Goulin Khoge <goulin.khoge@gmail.com>
2021-09-06 09:49:14 +0200

    Added meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import "../styles/date-picker.css"
-import "../styles/form.css"
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import "../styles/date-picker.css";
+import "../styles/form.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="theme-color" content="#007a66" />
+        <meta
+          name="description"
+          content="Trouver des salles vides pour les étudiants de l'Université Paul Sabatier"
+        />
+        <title>Salles vides</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
