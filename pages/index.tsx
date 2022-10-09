466cb6c93d7ae8e00dd18a8d36a9a6d8b2e35f45
Goulin Khoge <36013798+goulinkh@users.noreply.github.com>
2022-10-09 17:41:04 +0200

    add status widget & some styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,17 @@ import { PlanningForm } from "../components/PlanningForm";
 import { config } from "../config";
 import { BuildingsProvider } from "../providers/Buildings";
 
+const getScheduleColor = (startDate: string, endDate: string) => {
+  const availableHours =
+    (new Date(endDate).getTime() - new Date(startDate).getTime()) /
+    (1000 * 60 * 60);
+  const maxHours = 12.25;
+  const percentage = (availableHours * 100) / maxHours;
+  console.log(percentage);
+  const hue = Math.floor((percentage * 120) / 100); // go from green to red
+  return `hsl(${hue} 60% 28%)`;
+};
+
 const Home: NextPage = () => {
   const [freeRooms, setFreeRooms] = useState<
     { room: string; freeSchedules: { start: string; end: string }[] }[]
@@ -15,18 +26,24 @@ const Home: NextPage = () => {
 
   return (
     <BuildingsProvider>
-      <>
+      <div className="root">
         <Header />
-        <div className="container">
+        <main className="container">
           <PlanningForm onSubmit={onSearchSubmit} />
           <ul>
             {freeRooms && freeRooms.length
               ? freeRooms.map((r) => (
                   <li key={r.room}>
-                    <h2>{r.room}</h2>
+                    <p style={{ fontWeight: "bold" }}> {r.room}</p>
                     <ul>
                       {r.freeSchedules.map((s) => (
-                        <li key={s.start} style={{ margin: ".2rem 0" }}>
+                        <li
+                          key={s.start}
+                          style={{
+                            margin: ".3rem 0",
+                            color: getScheduleColor(s.start, s.end),
+                          }}
+                        >
                           {new Date(s.start).toLocaleTimeString(
                             navigator.language,
                             {
@@ -51,8 +68,18 @@ const Home: NextPage = () => {
                 ))
               : null}
           </ul>
-        </div>
-      </>
+        </main>
+        <footer className="container">
+          <iframe
+            src="https://rooms-finder.instatus.com/embed-status/light-sm"
+            width="230"
+            height="61"
+            frameBorder="0"
+            scrolling="no"
+            style={{ border: "none" }}
+          ></iframe>
+        </footer>
+      </div>
     </BuildingsProvider>
   );
 };
